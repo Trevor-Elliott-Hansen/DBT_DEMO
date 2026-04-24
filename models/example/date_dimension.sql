ed1918f814aa759c41835fa06b636af71402f0a7
Trevor Elliott-Hansen <trevor.elliott.hansen@gmail.com>
2026-04-24 17:06:16 +0000

    date utils and daily_weather aggregates

--- models/example/date_dimension.sql
+++ models/example/date_dimension.sql
@@ -1,6 +1,6 @@
 WITH CAST_CTE AS (
     SELECT
-        TRY_TO_TIMESTAMP(STARTED_AT, 'YYYY-MM-DD HH24:MI:SS.FF9') AS STARTED_AT
+        TRY_TO_TIMESTAMP(STARTED_AT, 'MM/DD/YYYY HH24:MI:SS') AS STARTED_AT
     FROM {{ source('demo', 'bike') }}
     WHERE STARTED_AT != 'stoptime'
 ),
@@ -10,22 +10,8 @@ CTE AS (
         STARTED_AT,
         DATE(STARTED_AT) AS DATE_STARTED_AT,
         HOUR(STARTED_AT) AS HOUR_STARTED_AT,
-        CASE
-            WHEN DAYNAME(STARTED_AT) IN ('Sat', 'Sun')
-            THEN 'WEEKEND'
-            ELSE 'BUSINESS DAY'
-        END AS DAY_TYPE,
-        MONTH(STARTED_AT),
-        CASE 
-            WHEN MONTH(STARTED_AT) IN (12,1,2)
-            THEN 'WINTER'
-            WHEN MONTH(STARTED_AT) IN (3,4,5)
-            THEN 'SPRING'
-            WHEN MONTH(STARTED_AT) IN (6,7,8)
-            THEN 'SUMMER'
-            WHEN MONTH(STARTED_AT) IN (9,10,11)
-            THEN 'FALL'
-        END AS SEASON
+        {{day_type('STARTED_AT')}} AS DAY_TYPE,
+        {{get_season('STARTED_AT')}} AS SEASON
 
     FROM CAST_CTE
 )
